7674e61142b408c6764e2216cd7c60bb124e375e
Adam Rustell <adamrustell@redweb.com>
2016-06-10 11:29:04 +0100

    Added gallery and fixed header

---
+++ js/gallery.js
@@ -0,0 +1,8 @@
+var preview = document.getElementsByClassName("main-preview");
+
+//$(".thumbnail").on("click", function() {
+//    if($(preview).attr("src") != $(this).attr("src")) {
+//        $(preview).attr("src") = $(this).attr("src");
+//        console.log("click");
+//    }
+//});
