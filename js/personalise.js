8bfca5b1520b095a387de84c8d774f781e67e649
Adam Rustell <adamrustell@redweb.com>
2016-06-14 16:59:01 +0100

    added some personalisation

---
+++ js/personalise.js
@@ -0,0 +1,10 @@
+var header = document.getElementById("title");
+
+if(sessionStorage.getItem("name") == null || '') {
+    var person = prompt("Hello, What is your name?", "Insert your name here.");
+    sessionStorage.setItem('name', person);
+} 
+
+header.innerHTML = "Welcome to my Portfolio-site " + sessionStorage.getItem('name');
+
+console.log(sessionStorage.getItem('name'));
